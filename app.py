6e855ab8d9e43b674cc4035834e13c52158bc20c
trosar <thomas.rosario@gmail.com>
2017-09-26 13:04:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def makeWebhookResult(req):
         else:
             status = "Not available"
             print "No match!!"
-        speech = "Order status is " + status + ". Thanks for visiting us today"
+        speech = "Order status is " + status + ". Thank you for visiting us today"
     else:
         return{}
     print("Response:")
