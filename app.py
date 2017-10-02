4dd74125d2cbb1ad8be8dc57a6fa1b1439e1cdd5
Harshit Sanghvi <harshit.sanghvi@gmail.com>
2017-10-02 17:54:22 -0400

    Development to Master Merge (#3)
    * Updating action Order_Status_yes (#2)
    
    - Updating the action name to Order_Status_yes
    - Adding if/else based on the status.
    
    * Correcting the error on line 60
    
    * Adding the order details action
    
    * Updating action for order details
    
    * Updating app.py
    
    * Update app.py
    
    * Adding parentheses to print statements
    
    * Updating app.py
    
    * updating app.py
    
    * Updating app.py
    
    * Update app.py
    
    * Update app.py
    
    * Adding date for Order Details
    
    * Updating date logic for Order Details
    
    * Updating date logic for Order Details
    
    * Updating date logic for order details
    
    * Updating date logic for Order Details
    
    * Updating date logic for Order Details
    
    * Updating date logic for Order Details
    
    * Updating date logic
    
    * Updating Order Details module
    
    * Adding the NO scenario for order details
    
    * Updating 'NO' logic for order details
    
    * Updating NO logic for order details
    
    * Updating NO logic for order details
    
    * Updating the NO logic for order details
    
    * Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import re
 from flask import Flask
 from flask import request
 from flask import make_response
+from datetime import datetime as DateTime, timedelta as TimeDelta
 
 
 # Flask app should start in global layout
@@ -35,11 +36,39 @@ def makeWebhookResult(req):
         parameters = result.get("parameters")
         color = parameters.get("color")
         cat = parameters.get("catalog-category")
-
-        rq = requests.get("http://www.lanebryant.com/lanebryant/search?Ntt=" + color + " " + cat + "&format=JSON")
+        if (((color is None) or (color is "")) and (req.get("originalRequest") is not None) and (req.get("originalRequest").get("source") == "facebook")):
+            return {
+                "data": {
+                    "facebook": {
+                        "text": "Pick a color:",
+                        "quick_replies": [
+                            {
+                                "content_type": "text",
+                                "title": "Red",
+                                "payload": "red"
+                            },
+                            {
+                                "content_type": "text",
+                                "title": "Green",
+                                "payload": "green"
+                            }
+                        ]
+                    }
+                }
+            }
+        else:
+            rq = requests.get("http://www.lanebryant.com/lanebryant/search?Ntt=" + color + " " + cat + "&format=JSON")
+            jdata = json.loads(rq.text)
+            speech = "I found " + str(jdata["contents"][0]["MainContent"][0]["MainContent"][0]["contents"][0]["totalNumRecs"]) + " " + color + " " + cat + " products." 
+    elif req.get("result").get("action") == "promos":
+        result = req.get("result")
+        headers = {'HOST': 'sit.catherines.com'}
+        rq = requests.get("https://23.34.4.174/static/promo_01?format=json", headers=headers, verify=False)
         jdata = json.loads(rq.text)
-        speech = "I found " + str(jdata["contents"][0]["MainContent"][0]["MainContent"][0]["contents"][0]["totalNumRecs"]) + " " + color + " " + cat + " products." 
-    elif req.get("result").get("action") == "checkout.order.status":
+        speech = "Promos are "
+        for mc in jdata["MainContent"]:
+            speech = speech + str(mc["freeFormContent"]) + "... "
+    elif ((req.get("result").get("action") == "Order_Status_yes") or (req.get("result").get("action") == "checkout.order.status")):
         result = req.get("result")
         parameters = result.get("parameters")
         zipcode = parameters.get("zipcode")
@@ -48,50 +77,78 @@ def makeWebhookResult(req):
         rq = requests.post("https://www.shopjustice.com/justice/homepage/includes/order-response-html.jsp", data={'orderNum': ordernum, 'billingZip': zipcode, 'Action': 'fetchODDetails'})
         #print rq.text
         matchObj = rq.text[rq.text.find("mar-status")+12:rq.text.find("<", rq.text.find("mar-status"))]
+        matchDate = rq.text[rq.text.find("mar-date")+10:rq.text.find("<", rq.text.find("mar-date"))]
         if len(matchObj) < 50:
+            print ("matchObj : ", matchObj)
+            print ("matchDate : ", matchDate)
             status = matchObj
-            print "matchObj : ", matchObj
+            date = DateTime.strptime(matchDate, '%m/%d/%Y') + TimeDelta(days=5)
         else:
-            status = "Not available"
-            print "No match!!"
-        speech = "Order status is " + status + ". Thank you for visiting us today"
+            status = "I couldn’t find that order. Either the number or the zipcode is not correct."
+            print ("No match!!")
+            
+        if status == 'Shipped':
+            speech = "Order status is " + status + ". You shall receive the package by " + date.strftime('%m/%d/%Y') + "."
+        elif status == 'Canceled':
+            speech = "Order status is " + status + ". Please reach out to the customer support for more details about the order."
+        elif status == 'Processing':
+            speech = "Order status is " + status + ". You shall receive the package by " + date.strftime('%m/%d/%Y') + "."
+        else:
+            speech = status
+    elif req.get("result").get("action") == "Order_Status_no":
+        result = req.get("result")
+        parameters = result.get("parameters")
+        zipcode = parameters.get("zipcode")
+        email = parameters.get("email")
+        ordertime = parameters.get("order-time")
+        
+        #TODO - Need to put here the API to get order number using email, zipcode and the timeframe
+        if zipcode == '20166':
+            ordernum = 'OJTW022967052'
+        elif zipcode == '37122':
+            ordernum = 'OJTW027567667'
+        elif zipcode == '19148':
+            ordernum = 'OJTW027678055'
+        else:
+            ordernum = 'OJTW022967052'
+            
+        rq = requests.post("https://www.shopjustice.com/justice/homepage/includes/order-response-html.jsp", data={'orderNum': ordernum, 'billingZip': zipcode, 'Action': 'fetchODDetails'})
+        #print rq.text
+        matchObj = rq.text[rq.text.find("mar-status")+12:rq.text.find("<", rq.text.find("mar-status"))]
+        matchDate = rq.text[rq.text.find("mar-date")+10:rq.text.find("<", rq.text.find("mar-date"))]
+        if len(matchObj) < 50:
+            print ("matchObj : ", matchObj)
+            print ("matchDate : ", matchDate)
+            status = matchObj
+            date = DateTime.strptime(matchDate, '%m/%d/%Y') + TimeDelta(days=5)
+        else:
+            status = "I couldn’t find that order. Either the number or the zipcode is not correct."
+            print ("No match!!")
+            
+        if status == 'Shipped':
+            speech = "Order status is " + status + ". You shall receive the package by " + date.strftime('%m/%d/%Y') + "."
+        elif status == 'Canceled':
+            speech = "Order status is " + status + ". Please reach out to the customer support for more details about the order."
+        elif status == 'Processing':
+            speech = "Order status is " + status + ". You shall receive the package by " + date.strftime('%m/%d/%Y') + "."
+        else:
+            speech = status
     else:
         return{}
     print("Response:")
     print(speech)
-    if ((req.get("originalRequest") is not None) and (req.get("originalRequest").get("source") == "facebook")):
-        return {
-            "data": {
-                "facebook": {
-                    "text": "Pick a color:",
-                    "quick_replies": [
-                        {
-                            "content_type": "text",
-                            "title": "Red",
-                            "payload": "red"
-                        },
-                        {
-                            "content_type": "text",
-                            "title": "Green",
-                            "payload": "green"
-                        }
-                    ]
-                }
-            }
-        }
-    else:
-        return {
-            "speech": speech,
-            "displayText": speech,
-            #"data": {},
-            # "contextOut": [],
-            "source": "apiai-onlinestore-search"
-        }
+    return {
+        "speech": speech,
+        "displayText": speech,
+        #"data": {},
+        # "contextOut": [],
+        "source": "apiai-onlinestore-search"
+    }
         
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
-    print "Starting app on port %d" % port
+    print ("Starting app on port %d" % port)
 
     app.run(debug=True, port=port, host='0.0.0.0')
