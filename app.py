90a217ab084540596d158f4c9329278210a461d3
Harshit Sanghvi <harshit.sanghvi@gmail.com>
2017-10-05 17:47:38 -0400

    Updating promo logic

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def makeWebhookResult(req):
                                 "template_type": "list",
                                 "elements": [{
                                     "title": str(jdata["MainContent"][0]["freeFormContent"]),
-                                    "image_url": "https://xvir.github.io/img/apiai.png",
+                                    "image_url": "",
                                     "default_action": {
                                         "type": "web_url",
                                         "url": "https://www.lanebryant.com/"
@@ -89,12 +89,7 @@ def makeWebhookResult(req):
                                     "default_action": {
                                         "type": "web_url",
                                         "url": "https://www.lanebryant.com/"
-                                    },
-                                    "buttons": [{
-                                        "title": "View",
-                                        "type": "web_url",
-                                        "url": "https://www.lanebryant.com/"
-                                    }]
+                                    }
                                 }]
                             }
                         }
