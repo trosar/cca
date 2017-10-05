e9034d89489ec6193e8fd046826b7e44d571cc13
Harshit Sanghvi <harshit.sanghvi@gmail.com>
2017-10-05 18:19:50 -0400

    Updating promo logic for FB

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def makeWebhookResult(req):
                                 "template_type": "list",
                                 "elements": [{
                                     "title": "LaneBryant Active Promotions",
-                                    "image_url": "https://xvir.github.io/img/apiai.png",
+                                    "image_url": "https://s7d2.scene7.com/is/image/lanebryantProdATG/092517-navBG-PE",
                                     "default_action": {
                                         "type": "web_url",
                                         "url": "http://www.lanebryant.com/"
