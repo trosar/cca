9d90435983ff5152424ab7556c6efbdc2b2aff36
Harshit Sanghvi <harshit.sanghvi@gmail.com>
2017-10-09 18:30:38 -0400

    Updating Order Status Receipt logic

--- app.py
+++ app.py
@@ -70,7 +70,20 @@ def makeWebhookResult(req):
         rq = requests.post("https://www.shopjustice.com/justice/homepage/includes/order-response-html.jsp", data={'orderNum': ordernum, 'billingZip': zipcode, 'Action': 'fetchODDetails'})
         
         order_json = json.loads(rq.text[rq.text.find("cart-json")+35:rq.text.find("<", rq.text.find("cart-json"))])
-        print (str(order_json))
+        #print (str(order_json))
+        
+        temp = "\"quantity\":1,\"price\":25,\"currency\":\"USD\",\"image_url\":\"http://petersapparel.parseapp.com/img/grayshirt.png\""
+            elements = ""
+            count = len(order_json["cartItems"])
+            
+            for mc in order_json["cartItems"]:
+                element = "{\"title\": " + "\"" + str(mc["name"]) + "\"," + temp
+                if(count != 1):
+                    element = element + ","
+                    count = count - 1
+                elements = elements + element
+            
+            json_elements = json.loads("["+elements+"]")
         
         if ((req.get("originalRequest") is not None) and (req.get("originalRequest").get("source") == "facebook")):
             return {
@@ -107,22 +120,7 @@ def makeWebhookResult(req):
                                     "name": "$10 Off Coupon",
                                     "amount": 10
                                 }],
-                                "elements": [{
-                                    "title": "Classic White T-Shirt",
-                                    "subtitle": "100% Soft and Luxurious Cotton",
-                                    "quantity": 2,
-                                    "price": 50,
-                                    "currency": "USD",
-                                    "image_url": "http://petersapparel.parseapp.com/img/whiteshirt.png"
-                                },
-                                {
-                                    "title": "Classic Gray T-Shirt",
-                                    "subtitle": "100% Soft and Luxurious Cotton",
-                                    "quantity": 1,
-                                    "price": 25,
-                                    "currency": "USD",
-                                    "image_url": "http://petersapparel.parseapp.com/img/grayshirt.png"
-                                }]
+                                "elements": json_elements
                             }
                         }
                     }
