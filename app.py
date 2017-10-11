7d5122af288fda7628ef7391f9155977dcd4c16c
Harshit Sanghvi <harshit.sanghvi@gmail.com>
2017-10-10 23:25:32 -0400

    Updating Order status receipt logic

--- app.py
+++ app.py
@@ -69,18 +69,20 @@ def makeWebhookResult(req):
         
         rq = requests.post("https://www.shopjustice.com/justice/homepage/includes/order-response-html.jsp", data={'orderNum': ordernum, 'billingZip': zipcode, 'Action': 'fetchODDetails'})
         
-        order_json = json.loads(rq.text[rq.text.find("cart-json")+35:rq.text.find("<", rq.text.find("cart-json"))])
-        #print (str(order_json))
+        jdata = rq.text[rq.text.find("cart-json")+35:rq.text.find("<", rq.text.find("cart-json"))]
+        jdata = jdata.replace('"', "'")
+        order_json = json.loads(jdata)
+        print (str(order_json))
         print (str(order_json["data"]["cartItems"]))
         
         temp = "\"quantity\":1,\"price\":25,\"currency\":\"USD\",\"image_url\":\"http://petersapparel.parseapp.com/img/grayshirt.png\""
         elements = ""
         
-        count = len(order_json["data"][0]["cartItems"])
+        count = len(order_json["data"]["cartItems"])
         print (count)
             
-        for mc in order_json["data"]:
-            element = "{\"title\": " + "\"" + str(mc[0]["cartItems"][0]["name"]) + "\"," + temp
+        for mc in order_json["data"]["cartItems"]:
+            element = "{\"title\": " + "\"" + str(mc[0]["name"]) + "\"," + temp
             if(count != 1):
                 element = element + ","
                 count = count - 1
